b8776eb3f603d06548155c7eb015afbba2cd2289
Alexander Shishkin <virtuoso@slind.org>
2007-09-10 16:32:29 +0400

    added means of inserting and updating records in overrides.db Signed-off-by: Alexander Shishkin <virtuoso@slind.org>

--- src/overrides.c
+++ src/overrides.c
@@ -26,6 +26,26 @@ struct fetch {
 	char ***data;
 };
 
+/*
+ * return " AND (arch='SOME_ARCH' OR arch='')" that is
+ * allocated and requires free()'ing
+ */
+static inline char *ov_arch_clause(char *arch)
+{
+	char *clause;
+	int s = 0;
+
+	if (*arch == '\0')
+		clause = arch;
+	else
+		s = asprintf(&clause, " AND (arch='%s' OR arch='')", arch);
+
+	if (s == -1)
+		return NULL;
+
+	return clause;
+}
+
 static int ov_fetch_cb(void *user, int cols, char **values, char **keys)
 {
 	char **out = (char **)user;
@@ -42,10 +62,125 @@ static int ov_fetch_cb(void *user, int cols, char **values, char **keys)
 	return GE_OK;
 }
 
+int ov_insert(char *pkgname, char *version, char *arch,
+		char *suite, char *component)
+{
+	char *req;
+	char *err;
+	int s;
+
+	req = sqlite3_mprintf(
+			"INSERT INTO overrides (" OV_COLS ") "
+			"VALUES ('%s', '%s', '%s', '%s', '%s')",
+			pkgname, version, suite, arch, component);
+
+	s = sqlite3_exec(db, req, NULL, NULL, &err);
+
+	if (s != SQLITE_OK) {
+		SHOUT("Error (%d) occured while updating the table: %s,\n"
+				"query was: \"%s\"\n", s, err ? err : "", req);
+
+		s = GE_ERROR;
+	} else
+		s = GE_OK;
+
+	sqlite3_free(req);
+
+	return s;
+}
+
+int ov_update_version(char *pkgname, char *arch, char *suite, char *version)
+{
+	char *req;
+	char *err;
+	char *arch_clause = arch;
+	int s;
+
+	GE_ERROR_IFNULL(pkgname);
+	GE_ERROR_IFNULL(arch);
+	GE_ERROR_IFNULL(suite);
+	GE_ERROR_IFNULL(version);
+
+	if (*arch) {
+		s = asprintf(&arch_clause, " AND arch='%s'", arch);
+		if (s == -1)
+			return GE_ERROR;
+	}
+
+	req = sqlite3_mprintf(
+			"UPDATE overrides SET version='%s' "
+			"WHERE pkgname='%s' "
+			"AND suite='%s'%s",
+			version, pkgname, suite, arch_clause);
+
+	if (arch_clause != arch)
+		free(arch_clause);
+
+	s = sqlite3_exec(db, req, NULL, NULL, &err);
+
+	if (s != SQLITE_OK) {
+		SHOUT("Error (%d) occured while updating the table: %s,\n"
+				"query was: \"%s\"\n", s, err ? err : "", req);
+
+		s = GE_ERROR;
+	} else
+		s = GE_OK;
+
+	sqlite3_free(req);
+
+	return s;
+}
+
+int ov_update_suite(char *pkgname, char *version, char *arch,
+		char *from_suite, char *to_suite)
+{
+	char *req;
+	char *err;
+	char *arch_clause = arch;
+	int s;
+
+	GE_ERROR_IFNULL(pkgname);
+	GE_ERROR_IFNULL(arch);
+	GE_ERROR_IFNULL(from_suite);
+	GE_ERROR_IFNULL(to_suite);
+	GE_ERROR_IFNULL(version);
+
+	if (*arch) {
+		s = asprintf(&arch_clause, " AND arch='%s'", arch);
+		if (s == -1)
+			return GE_ERROR;
+	}
+
+	req = sqlite3_mprintf(
+			"UPDATE overrides SET suite='%s' "
+			"WHERE pkgname='%s' "
+			"AND suite='%s' "
+			"AND version='%s'%s",
+			to_suite, pkgname, from_suite, version, arch_clause);
+
+	if (arch_clause != arch)
+		free(arch_clause);
+
+	s = sqlite3_exec(db, req, NULL, NULL, &err);
+
+	if (s != SQLITE_OK) {
+		SHOUT("Error (%d) occured while updating the table: %s,\n"
+				"query was: \"%s\"\n", s, err ? err : "", req);
+
+		s = GE_ERROR;
+	} else
+		s = GE_OK;
+
+	sqlite3_free(req);
+
+	return s;
+}
+
 int ov_search(char *where, int order, char **data)
 {
 	char *req;
 	char *err;
+	int s;
 
 	data[0] = NULL;
 	req = sqlite3_mprintf(
@@ -53,10 +188,19 @@ int ov_search(char *where, int order, char **data)
 			"WHERE %s ORDER BY %s %s LIMIT 1",
 			where, ORDER_BY, ORDER_DIR);
 
-	sqlite3_exec(db, req, ov_fetch_cb, data, &err);
+	DBG("sql req: \"%s\"\n", req);
+	s = sqlite3_exec(db, req, ov_fetch_cb, data, &err);
+	if (s != SQLITE_OK) {
+		SHOUT("Error (%d) occured while selecting: %s,\n"
+				"query was: \"%s\"\n", s, err ? err : "", req);
+
+		s = GE_ERROR;
+	} else
+		s = (data[0] ? GE_OK : GE_EMPTY);
+
 	sqlite3_free(req);
 
-	return (!data[0] ? GE_EMPTY : GE_OK);
+	return s;
 }
 
 /*
@@ -67,6 +211,7 @@ int ov_find_component(char *pkgname, char *version, char *arch, char *suite,
 {
 	char *req;
 	char *err;
+	char *arch_clause;
 	char *data[OV_NCOLS];
 	int s;
 
@@ -74,14 +219,20 @@ int ov_find_component(char *pkgname, char *version, char *arch, char *suite,
 	GE_ERROR_IFNULL(version);
 	GE_ERROR_IFNULL(arch);
 	GE_ERROR_IFNULL(suite);
+ 
+	arch_clause = ov_arch_clause(arch);
+	GE_ERROR_IFNULL(arch_clause);
 
 	asprintf(&req,
 			"pkgname='%s' "
 			"AND version='%s' "
-			"AND suite='%s' "
-			"AND (arch='%s' OR arch='')", /* ??? */
-			pkgname, version, suite, arch
+			"AND suite='%s'"
+			"%s",
+			pkgname, version, suite, arch_clause
 		);
+
+	if (arch_clause != arch)
+		free(arch_clause);
 
 	GE_ERROR_IFNULL(req);
 
@@ -103,19 +254,26 @@ int ov_find_suite(char *pkgname, char *version, char *arch,
 {
 	char *req;
 	char *err;
+	char *arch_clause;
 	char *data[OV_NCOLS];
 
 	GE_ERROR_IFNULL(pkgname);
 	GE_ERROR_IFNULL(version);
 	GE_ERROR_IFNULL(arch);
 
+	arch_clause = ov_arch_clause(arch);
+	GE_ERROR_IFNULL(arch_clause);
+
 	asprintf(&req,
 			"pkgname='%s' "
 			"AND version='%s' "
-			"AND suite='%s' "
-			"AND (arch='%s' OR arch='')", /* ??? */
-			pkgname, version, suite, arch
+			"AND suite='%s'"
+			"%s",
+			pkgname, version, suite, arch_clause
 		);
+
+	if (arch_clause != arch)
+		free(arch_clause);
 
 	GE_ERROR_IFNULL(req);
 
@@ -132,22 +290,33 @@ int ov_find_version(char *pkgname, char *arch, char *suite, char **version)
 	char *req;
 	char *err;
 	char *data[OV_NCOLS];
+	char *arch_clause;
+	int s;
 
 	GE_ERROR_IFNULL(pkgname);
 	GE_ERROR_IFNULL(arch);
 	GE_ERROR_IFNULL(suite);
 
+	arch_clause = ov_arch_clause(arch);
+	GE_ERROR_IFNULL(arch_clause);
+
 	asprintf(&req,
 			"pkgname='%s' "
-			"AND suite='%s' "
-			"AND (arch='%s' OR arch='')",
-			pkgname, suite, arch
+			"AND suite='%s'"
+			"%s",
+			pkgname, suite, arch_clause
 		);
+
+	if (arch_clause != arch)
+		free(arch_clause);
 
 	GE_ERROR_IFNULL(req);
 
-	ov_search(req, -OV_ARCH, data);
+	s = ov_search(req, -OV_ARCH, data);
 	free(req);
+
+	if (s != GE_OK)
+		return s;
 
 	*version = strdup(data[OV_VERSION]);
 
