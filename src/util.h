43665ac3d7e48f1c9f7b972b3a43a5329ea6be58
Alexander Shishkin <virtuoso@slind.org>
2007-09-20 18:10:12 +0400

    add xmalloc()/xfree() macros wrapping around malloc() and free() Signed-off-by: Alexander Shishkin <virtuoso@slind.org>

--- src/util.h
+++ src/util.h
@@ -17,5 +17,19 @@ int mkdir_p(char *dst, mode_t mode);
 
 void root_squash();
 
+#define xmalloc(__s) ({                    \
+                void *__ret = malloc(__s); \
+                if (!__ret)                \
+                        return GE_ERROR;   \
+                __ret;                     \
+        })
+
+#define xfree(__p) do {                      \
+                void **__P = (void *)&(__p); \
+                if (*__P)                    \
+                        free(*__P);          \
+                *__P = NULL;                 \
+        } while (0)
+
 #endif
 
